c2dcc947dfde13fe3b5a68726311da8c79d43aff
Grégoire Pineau <lyrixx@lyrixx.info>
2021-11-08 14:50:15 +0100

    Revert "Cancel the website"
    This reverts commit c54ec93d7891b58f070aa9cfb4406aef76028d0e.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+git diff --exit-code > /dev/null
+rc=$?
+
+if [[ $rc == 0 ]]; then
+    echo "nothing changed"
+    exit
+fi
+
+set -e
+
+echo "deploying"
+
+git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git pull github ${GITHUB_REF} --ff-only
+
+git add docs/
+
+git config --global user.email "lyrixx@lyrixx.info"
+git config --global user.name "Grégoire Pineau"
+git commit -m "Update live website"
+git push github HEAD:${GITHUB_REF}
