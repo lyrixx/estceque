d66d30d8bb074f6813dbc6940fb9e24b998cf2f3
Grégoire Pineau <lyrixx@lyrixx.info>
2021-11-09 11:49:55 +0100

    Also commit data.csv

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ echo "deploying"
 git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git pull github ${GITHUB_REF} --ff-only
 
-git add docs/
+git add docs/ data.csv
 
 git config --global user.email "lyrixx@lyrixx.info"
 git config --global user.name "Grégoire Pineau"
